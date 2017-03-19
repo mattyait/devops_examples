fab8953b88ec5c7ba06398c00af4cb5e34e5be7d
mattyait <mattyait@gmail.com>
2017-03-19 14:10:33 +0530

    Update sample.rb

--- AWS-SDK/sample.rb
+++ AWS-SDK/sample.rb
@@ -5,8 +5,8 @@ require_relative 'lib/ec2/ec2_instance'
 include Credential
 include Ec2Instance
 
-cred = get_credential('firstfuel')
-ids=instance_id(cred,"us-east-1","Name","qaperf-*")
+cred = get_credential('profile')
+ids=instance_id(cred,"us-east-1","Name","qa-*")
 ids.each do |i|
   puts i
 end
